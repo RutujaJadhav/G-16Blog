92ec7ed71851dcf76356fe7a92aedf5058164eb6
RutujaJadhav <jadhav.jitendra2014@vit.ac.in>
2016-08-07 23:33:48 +0530

    Location changes

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -144,13 +144,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
--- src/blog/wsgi.py
+++ src/blog/wsgi.py
@@ -11,11 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
-
+application = get_wsgi_application()
